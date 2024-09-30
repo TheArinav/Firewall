2b9fa827151a387cfc3623ee4a185ce95e4b2666
TheArinav <arieli6aya@gmail.com>
2024-09-30 09:24:24 +0300

    init

---
+++ Firewall/Firewall/ViewModels/MainViewModel.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Firewall.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+}
---
+++ Firewall/Firewall/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Firewall.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
