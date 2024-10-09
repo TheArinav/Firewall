e897f7300a4509ebb27b50bde71dee3164e6ffc8
TheArinav <arieli6aya@gmail.com>
2024-10-09 13:49:37 +0300

    Fixed unix related rendering issues

--- Firewall/Firewall.Desktop/Program.cs
+++ Firewall/Firewall.Desktop/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Avalonia;
+using Avalonia.OpenGL;
 
 namespace Firewall.Desktop;
 
@@ -13,9 +14,48 @@ sealed class Program
         .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    private static AppBuilder BuildAvaloniaApp()
+    {
+        return AppBuilder.Configure<App>()
+            .UseSkia()
             .UsePlatformDetect()
+            .With(new X11PlatformOptions
+            {
+                OverlayPopups = true,
+                UseDBusMenu = false,
+                UseDBusFilePicker = false,
+                EnableIme = true,
+                EnableInputFocusProxy = true,
+                EnableSessionManagement = true,
+                RenderingMode = new[]
+                {
+                    X11RenderingMode.Software,
+                    X11RenderingMode.Egl,
+                },
+                ShouldRenderOnUIThread = true,
+                GlProfiles = new[]
+                {
+                    new GlVersion(GlProfileType.OpenGL, 3, 2), 
+                    new GlVersion(GlProfileType.OpenGLES, 3, 0) 
+                },
+                GlxRendererBlacklist = new[]
+                {
+                    "llvmpipe" 
+                },
+                WmClass = "FirewallApp",
+                EnableMultiTouch = false,
+                UseRetainedFramebuffer = false
+            }).With(new Win32PlatformOptions
+            {
+                OverlayPopups = true, // Enable overlay popups
+                RenderingMode = new[]
+                {
+                    Win32RenderingMode.AngleEgl // Fallback to software rendering
+                },
+                ShouldRenderOnUIThread = true, // Enable rendering on the UI thread
+                DpiAwareness = Win32DpiAwareness.PerMonitorDpiAware // Set DPI awareness
+            })
             .WithInterFont()
             .LogToTrace();
+    } 
 }
